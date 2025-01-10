760760d7edc48a361544f84c2aefd905dd7234a0
Ryan Choi <ryan04122005@gmail.com>
2025-01-09 18:44:39 -0800

    integrate Data Dragon API to display LoL champions

--- myApp/models.py
+++ myApp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Champion(models.Model):
+    name = models.CharField(max_length=50)
+    icon = models.URLField()
+    gender = models.CharField(max_length=10)
+    position = models.CharField(max_length=20)
+    species = models.CharField(max_length=50)
+    resource = models.CharField(max_length=20)
+    range_type = models.CharField(max_length=20)
+    region = models.CharField(max_length=50)
+    release_year = models.IntegerField()
--- myApp/views.py
+++ myApp/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
+import requests
 
-# Create your views here.
+#Create your views here.
+#Test home page rendering champion names, title, and png image
 def home(request):
-    return render(request, 'home.html', {})
+    api_url = "https://ddragon.leagueoflegends.com/cdn/15.1.1/data/en_US/champion.json"
+    response = requests.get(api_url)
+    if response.status_code == 200:
+        champions_data = response.json()
+        champions = [
+            {   
+                #Get the name through the key of the dictionary
+                "name": name,
+                #Get the champion title from the value of the dictionary
+                "title": details["title"],
+                #In the Data Dragon API for LoL champions, the images of the champions are located here:
+                #We just have to pass in the name of the champion to get the image!
+                "image": f"https://ddragon.leagueoflegends.com/cdn/15.1.1/img/champion/{name}.png"
+            }
+            for name, details in champions_data['data'].items()
+        ]
+    else:
+        champions = []
+    return render(request, 'home.html', {'champions': champions})
 
