1f6fc8c725552fe0d30286db76c85d5b2dbe49a2
Ryan Choi <ryan04122005@gmail.com>
2025-04-26 23:02:52 -0700

    Register user-tracking in admin panel

--- myApp/admin.py
+++ myApp/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group, User
-from .models import Profile
+from .models import Profile, ActiveUserLog
 
 # Register your models here.
 admin.site.unregister(Group)
@@ -15,3 +15,8 @@ class UserAdmin(admin.ModelAdmin):
 
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
+
+@admin.register(ActiveUserLog)
+class ActiveUserLogAdmin(admin.ModelAdmin):
+    list_display = ('ip_address', 'date', 'user')
+    list_filter = ('date',)
